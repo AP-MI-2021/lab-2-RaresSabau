faaa458fa158741a6d9d5d18b06778672045c845
Sabau Rares <rares.sabau@stud.ubbcluj.ro>
2021-10-24 20:07:37 +0300

    rezolvare eroare

--- main.py
+++ main.py
@@ -1,94 +1,78 @@
-#PROBLEMELE 2, 3 SI 13
+#Problemele 2,3,13
 
 from datetime import datetime, date
+
 
 '''
 Problema 2
 '''
-
-
-def get_age_in_days():
+def get_age_in_days(datan: str):
     '''
-
-    :param zile: parametrul in care se salveaza varsta in zile
-    :return: vasrta persoanei in zile
-    '''
-    data_curenta = date.today()
-    zile = int (data_curenta) - int (data_nastere)
-    return zile
-
-def data_nasterii(datan: str):
-    '''
-    Functie folosita pentru a transforma data citita de tip str in .date()
+    Functia returneaza varsta persoanei in zile
+    :param datan:parametru prin care se calculeaza data nasterii din str in .date()
+    :return:
     '''
 
     data_nastere = datetime.strptime(datan, '%d/%m/%y')
 
     data_nastere = data_nastere.date()
 
-    return data_nastere
+    return (date.today() - data_nastere).days
 
-
-
-
+def test_get_age_in_days():
+    assert (get_age_in_days("06/08/02")) == 7019
+    assert (get_age_in_days('27/2/96')) == 9371
 
 '''
 Problema 13
 '''
-def get_temp(temp: float, scara1: str, scara2: str):
+def get_temp(temp, scara1, scara2):
     """
     :param temp: primeste temperatura
     :param scara1: scara initiala
     :param scara2: scara in care se doreste sa fie transformata temperatura
     :return: returneaza valoarea temperaturii din scara1 in scara2
     """
-
-    if scara1 == 'C':
-        if scara2 == 'K':
+    temp = int (temp)
+    if scara1 == 'c':
+        if scara2 == 'k':
             return temp + 273.15
-        elif scara2 == 'F':
+        elif scara2 == 'f':
             return temp * 1.8 + 32
-    elif scara1 == 'K':
-        if scara2 == 'C':
+    elif scara1 == 'k':
+        if scara2 == 'c':
             return temp - 273.15
-        elif scara2 == 'F':
+        elif scara2 == 'f':
             return (temp - 32) / 1.8 + 273.15
-    elif scara1 == 'F':
-        if scara2 == 'C':
+    elif scara1 == 'f':
+        if scara2 == 'c':
             return (temp - 32) / 1.8
-        elif scara2 == 'K':
+        elif scara2 == 'k':
             return (temp - 32) * 5 / 9 + 273.15
 
     return temp
-
-
 def test_get_temp():
     assert (round(get_temp(300, 'k', 'c'), 2)) == 26.85
     assert (round(get_temp(16, 'c', 'f'), 2)) == 60.8
     assert (round(get_temp(45, 'f', 'k'), 2)) == 280.37
 
-
-test_get_temp()
-
-
 '''
 Problema 3
-
 '''
 
-def is_prime(n):
-    """
-       :param n:  numar natural
-       :return: True daca numarul n este prim si False daca numarul n nu este prim
-    """
-    for i in range(2, n//2):
+def prim(n):
+    '''
+    Returneaza True daca numarul este prim, iar Flase in caz contrar
+    :return:
+    '''
+    for i in range(2, n // 2):
         if n % i == 0:
             return False
     return True
 
-def test():
-    assert is_prime(3) == True
-    assert is_prime(4) == False
+def test_prim():
+    assert prim(7) == True
+    assert prim(82) == False
 
 
 def get_goldbach(n):
@@ -98,8 +82,8 @@ def get_goldbach(n):
     """
 
     for i in range(2, n):
-        if is_prime(i):
-            if is_prime(n - i):
+        if prim(i):
+            if prim(n - i):
                 return i, n - i
     return False
 
@@ -108,36 +92,45 @@ def test_get_goldbach():
     assert get_goldbach(16) == (3, 13)
     assert get_goldbach(15) == (2, 13)
 
-def meniu():
-    print('''
-1.Problema 2
-2.Problema 13
-3.Problema 3
-4.Afara''')
-
+def show_menu():
+    '''
+    Printare meniu
+    :return:
+    '''
+    print ('''
+    1. Problema 2
+    2. Problema 13.
+    3. Problema 3
+    x. Iesire
+    ''')
 
 def main():
-
-
     while True:
-        meniu()
-        cmd = input("Comanda:")
+        show_menu()
+        cmd = input ("Introduceti comanda dvs: ")
         if cmd == '1':
-            data1 = input("introduceti data nasterii in format zz/ll/aa")
-            data_nasterii (data1)
-            print (get_age_in_days())
+            datan = input ("Introduceti data nasterii: ")
+            print (get_age_in_days(datan))
+
         elif cmd == '2':
-            temp = input ("Introduceti temperatura:")
-            scara_initiala = input ("Introduceti scara initiala:")
-            scara_dorita = input ("Introduceti scara dorita:")
-            print(get_temp(temp, scara_initiala, scara_dorita))
+            temp = int (input ("Introduceti temperatura: "))
+            scara1 = input ("Introduceti scara temperaturii introduse: ")
+            scara2 = input ("Introduceti scara in care doriti sa fie transformata temperatura: ")
+            print (round(get_temp(temp, scara1, scara2)))
         elif cmd == '3':
-            n = int(input("introduceti un numar intreg n"))
+            n = int(input("introduceti un numar intreg n: "))
             print(get_goldbach(n))
-        elif cmd == '4':
+        elif cmd == 'x':
             break
         else:
-            print('comanda invalida')
+            print ("Comanda invalida")
+
+def run_tests():
+    test_get_temp()
+    test_get_age_in_days()
+    test_prim()
+    test_get_goldbach()
 
 if __name__ == '__main__':
+    run_tests()
     main()
